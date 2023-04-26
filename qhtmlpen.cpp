f23c4b21d98b42869f4f71049cf4dd7177691e2e
isDamont <program-pavel@mail.ru>
2023-04-26 15:33:10 +0800

    added logic for start render window

--- qhtmlpen.cpp
+++ qhtmlpen.cpp
@@ -8,11 +8,24 @@ QHTMLPen::QHTMLPen(QWidget *parent)
     ui->setupUi(this);
 
     textEdit = new QTextEdit(this);
+
     setCentralWidget(textEdit);
 }
 
 QHTMLPen::~QHTMLPen()
 {
     delete ui;
+}
+
+
+void QHTMLPen::Slot_render_Init()
+{
+    if(windowHTML)
+    {
+        delete windowHTML;
+    }
+    windowHTML = new WindowHtmlRender(this);
+    windowHTML -> updateRender(textEdit);
+    windowHTML -> show();
 }
 
--- qhtmlpen.h
+++ qhtmlpen.h
@@ -1,6 +1,7 @@
 #ifndef QHTMLPEN_H
 #define QHTMLPEN_H
 
+#include "windowhtmlrender.h"
 #include <QMainWindow>
 #include <QTextEdit>
 
@@ -16,8 +17,11 @@ public:
     QHTMLPen(QWidget *parent = nullptr);
     ~QHTMLPen();
 
+    void Slot_render_Init();
+
 private:
     Ui::QHTMLPen *ui;
     QTextEdit *textEdit;
+    WindowHtmlRender *windowHTML = nullptr;
 };
 #endif // QHTMLPEN_H
